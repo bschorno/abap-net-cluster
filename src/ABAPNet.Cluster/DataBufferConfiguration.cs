a9cac95430d197a50b05c3f8f5c7a08b5afa1e19
Bruno Schorno <bruno.schorno@hotmail.com>
2024-04-25 07:38:20 +0200

    Adding DATA BUFFER.docx

--- src/ABAPNet.Cluster/DataBufferConfiguration.cs
+++ src/ABAPNet.Cluster/DataBufferConfiguration.cs
@@ -5,6 +5,9 @@
         private readonly CodePage _codePage;
         private readonly Endian _endian;
 
+        private readonly bool _importIgnoreMissingSegments;
+        private readonly bool _importIgnoreSegmentTypeMismatch;
+
         public CodePage CodePage
         {
             get { return _codePage; }
@@ -15,6 +18,18 @@
         {
             get { return _endian; }
             init { _endian = value; }
+        }
+
+        public bool ImportIgnoreMissingSegments
+        {
+            get { return _importIgnoreMissingSegments; }
+            init { _importIgnoreMissingSegments = value; }
+        }
+
+        public bool ImportIgnoreSegmentTypeMismatch
+        {
+            get { return _importIgnoreSegmentTypeMismatch; }
+            init { _importIgnoreSegmentTypeMismatch = value; }
         }
 
         public DataBufferConfiguration(CodePage codePage, Endian endian)
